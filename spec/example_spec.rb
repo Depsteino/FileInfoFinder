fed13c8535153d6032916f5e8355009109f2a1ff
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 01:05:47 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -4,6 +4,8 @@ require_relative '../main.rb'
 
   describe '#FindPath' do
  it 'should read YAML-formatted data from a file' do
+     allow_any_instance_of(Object).
+      to receive(:FindPath).with('mydata.txt')
     
     expect(FindPath('../mydata.txt')).to eq "mydata.txt"
 end
