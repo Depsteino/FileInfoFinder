ed68cb074ba55eec5dbfc265e23fc3fb554f8c27
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 00:46:11 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 require_relative '../main.rb'
 
   describe '#FindPath' do
-    it "does something" do
-  arg = "../mydata.txt"
-  file = File.expand_path('../mydata.txt', File.dirname(__FILE__))
-      put "test"
-end
-end
+
+  end
 
