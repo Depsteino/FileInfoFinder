d024d6c6ad4113a09f389b095eb7f0ca00bce123
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 00:26:26 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 require_relative '../main.rb'
 
   describe '#FindPath' do
-  it 'Given a path like "/home/kim/mydata.txt", return mydata.txt' do
-    expect { ruby ../main.rb mydata.txt) }.to output("mydata.txt").to_stdout
-  end
+     ruby ../main.rb mydata.txt
 end
 
