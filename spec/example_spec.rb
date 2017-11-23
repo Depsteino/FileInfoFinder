d78857521ad6322fc8e615ee630715eb08f0beaa
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 00:34:20 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 require_relative '../main.rb'
 
   describe '#FindPath' do
-     ruby ../main.rb mydata.txt
+    it "does something" do
+  arg = "/tmp"
+  exe = File.expand_path('../bin/site_checker', File.dirname(__FILE__))
+  stdin, stdout, stderr = Open3.popen3("#{exe} -Ilib #{arg}")
+  stdout.readlines.should be_empty
+end
 end
 
