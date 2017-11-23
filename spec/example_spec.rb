c10eb0b204553ab292d840eb9e9dad1269895791
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 00:38:49 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -3,10 +3,8 @@ require_relative '../main.rb'
 
   describe '#FindPath' do
     it "does something" do
-  arg = "/tmp"
-  exe = File.expand_path('../mydata.txt', File.dirname(__FILE__))
-  stdin, stdout, stderr = Open3.popen3("#{exe} -Ilib #{arg}")
-  stdout.readlines.should be_empty
+  arg = "../mydata.txt"
+  file = File.expand_path('../mydata.txt', File.dirname(__FILE__))
 end
 end
 
