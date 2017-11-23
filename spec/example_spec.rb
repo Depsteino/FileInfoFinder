9257dd7665f5cf329e3d512ed7126275987ff4b0
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 00:17:17 -0500

    Update example_spec.rb

--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -3,6 +3,11 @@ require_relative '../main.rb'
 
   describe '#FindPath' do
   it 'Given a path like "/home/kim/mydata.txt", return mydata.txt' do
-    expect { FindPath("test") }.to output("mydata.txt").to_stdout
+    expect { FindPath("") }.to output("mydata.txt").to_stdout
   end
+end
+
+if $0 == __FILE__
+  raise ArgumentError, "Usage: #{$0} <filename>" unless ARGV.length == 1
+  process(ARGV[0])
 end
