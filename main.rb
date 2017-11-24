98b073c03a14e9598e208eb20e3fd10f16baa1f7
Dan Epstein <25646665+Securter@users.noreply.github.com>
2017-11-23 19:58:35 -0500

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 
 require_relative 'lib/findInfo.rb'  
 
-fileArg= ""
+fileArg= "mydata.txt"
 
 # Task 1 filename without path. 
 # For example: given a path like "/home/kim/mydata.txt", return "mydata.txt"
@@ -21,6 +21,6 @@ DigestFileSha1(fileArg)
 
 # Task 4 MD5 digest for a file at the given path. 
 # For example, given a path like "/home/kim/mydata.txt", 
-# which contains the text The quick brown fox jumps over the lazy dog, 
+# which contains the text The quick brown fox jumps over the lazy dog 
 # return 9e107d9d372bb6826bd81d3542a419d6
 DigestFileMd5(fileArg)
